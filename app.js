bf5ce5ada4345efbd5b537ee6efb2f4ef42b50f5
Abhishek <67749693+abhishake21@users.noreply.github.com>
2022-06-04 20:18:57 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,26 +1,48 @@
 const dateOfBirth = document.querySelector('#date-of-birth');
 const luckyNumber = document.querySelector('#lucky-number');
 const checkBtn = document.querySelector('.check-btn');
-const result = document.querySelector('.result');
+const message = document.querySelector('.message');
+const gif = document.querySelector('.gif');
+
+gif.style.display = "none";
+message.style.display = "none";
 
 checkBtn.addEventListener('click', luckyOrNot);
 
 function luckyOrNot() {
     const dob = dateOfBirth.value;
     const luckyNum = luckyNumber.value;
-    
-    if (sum(dob) % luckyNum === 0) {
-        result.innerText = 'Your Birthday is Lucky.'
+
+    if (dob && luckyNum) {
+        checkLuckyOrNot(dob, luckyNum);
     } else {
-        result.innerText = 'Not lucky.'
+        gif.style.display = "none";
+        message.style.display = "block";
+        message.innerText = 'Enter both fields.'
     }
 }
 
 function sum(dob) {
     dob = dob.replaceAll('-','');
-    let sum = 0;
+    let total = 0;
     for (let i=0; i<dob.length; i++) {
-        sum = sum + Number(dob.charAt(i));
+        total = total + Number(dob.charAt(i));
     }
-    return sum;
+    return total;
+}
+
+function checkLuckyOrNot(dob, luckyNum) {
+    if (sum(dob) % luckyNum === 0) {
+        message.innerHTML = 'Your Birthday is <span>Lucky</span>';
+        gif.style.display = "block";
+        message.style.display = "block";
+        gif.src = './Images/lucky-gif.webp';
+        gif.alt = 'Party scene from "The Office"'
+    } else {
+        message.innerHTML = 'Your Birthday is not <span>Lucky</span>';
+        gif.style.display = "block";
+        message.style.display = "block";
+        gif.src = './Images/unlucky-gif.webp';
+        gif.alt = 'A man screaming "Unlucky"';
+    }
 }
